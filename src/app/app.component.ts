9718d68a9f6ba0ee4be1b14f59f93f44d7bdcc61
Ryan Transfiguracion <rtransfig@gmail.com>
2016-12-21 15:59:47 -0800

    - first commit - added static list of articles (37 minute mark of video)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { 
+  Component,
+  Input 
+} from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  template: `
+  <div id="sidebar">
+    Sidebar will go here
+  </div>
+  `
+})
+export class SidebarComponent
+{
+
+}
+
+@Component({
+  selector: "app-article",
+  template: `
+    <div>
+      <h2>{{ article.title }}</h2>
+    </div>
+  `
+})
+export class ArticleComponent
+{
+  @Input() article: Object;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `
+    <div id="container">
+      <app-sidebar></app-sidebar>   
+      <div id="content">
+        <app-article
+          *ngFor="let article of articles"
+          [article]="article">
+        </app-article>
+      </div>
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  articles: Object[];
+
+  constructor() 
+  {
+    this.articles = [
+      {
+        title: 'The Angular 2 Screenast',
+        description: 'The easiest way to learn Angular 2 is with Fullstack.io!'
+      },
+      {
+        title: 'Fullstack React',
+        description: 'Want to learn React too?'
+      },
+      {
+        title: 'Vue is new',
+        description: 'And pretty cool syntax too'
+      },
+      {
+        title: 'But what about elm?',
+        description: 'Everybody likes elm...right?'
+      }
+    ];
+  }
 }
